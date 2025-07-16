83947b71d528481e6c92fc276c0fdd3270e2b61d
Maxwell Lou <maxlou2005@gmail.com>
2025-07-15 21:34:43 -0400

    Accidentally used little-endian instead of big-endian when assigning bits to the input. Add more asserts throughout to help with debugging

--- test/test.py
+++ test/test.py
@@ -34,11 +34,14 @@ async def test_project(dut):
 
     dut._log.info("Test project behavior")
 
-    # Begin the test by inputting start (indicate to ALU that we want to start inputting numbers)
-    dut.uio_in.value = BinaryValue("0x1zzzzz")  # Set io[0] = 0 for add, leave the output ones to be driven by design
+    # Begin the test by inputting start (io[2]) (indicate to ALU that we want to start inputting numbers)
+    dut.uio_in.value = BinaryValue("zzzzz1x0")  # Set io[0] = 0 for add, leave the output ones to be driven by design
 
     await RisingEdge(dut.clk)
     await ReadWrite()
+
+    # Reset start to 0
+    dut.uio_in.value = BinaryValue("zzzzz0x0")
 
     # Numbers to add
     a = 1.234
@@ -47,17 +50,23 @@ async def test_project(dut):
 
     # Load operand A
     for i in range(4):
+        assert dut.state_out.value.integer == i + 1
+        assert dut.done.value == 0
         dut.uio_in.value = struct.pack("<f", a)[i]  # Put in little endian to input byte 0 first
         await RisingEdge(dut.clk)
         await ReadWrite()
 
     # Load operand B
     for i in range(4):
+        assert dut.state_out.value.integer == i + 5
+        assert dut.done.value == 0
         dut.uio_in.value = struct.pack("<f", b)[i]
         await RisingEdge(dut.clk)
         await ReadWrite()
 
     # Wait for execute
+    assert dut.state_out.value.integer == 9
+    assert dut.done.value == 0
     await RisingEdge(dut.clk)
     await ReadWrite()
 
@@ -65,10 +74,16 @@ async def test_project(dut):
     result = bytearray([0, 0, 0, 0])
     for i in range(4):
         result[i] = dut.uo_out.value.integer
+        assert dut.state_out.value.integer == i + 10
         assert dut.done.value == 1
         await RisingEdge(dut.clk)
         await ReadWrite()
 
+    # Make sure returned to IDLE state
+    assert dut.state_out.value.integer == 0
+    assert dut.done.value == 0
+
+    # Check result
     final_number = struct.unpack("<f", result)[0]
     temp = struct.unpack(">f", result)[0]
     assert abs(final_number - expected) < 1e-6, f"Addition failed: {a} + {b} != {final_number}. {result=}, big-endian:{temp}"
