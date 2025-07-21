08b92b4f0038267fe152fdfe6cf09e0588885a7d
Maxwell Lou <maxlou2005@gmail.com>
2025-07-20 22:25:20 -0400

    Fix integration tests. Need to attach power and ground lines, and for some reason await ReadWrite() no longer works?

--- test/README.md
+++ test/README.md
@@ -7,6 +7,9 @@ See below to get started or for more information, check the [website](https://ti
 
 1. Edit [Makefile](Makefile) and modify `PROJECT_SOURCES` to point to your Verilog files.
 2. Edit [tb.v](tb.v) and replace `tt_um_example` with your module name.
+3. Make a Python virtual environment and do `pip install -r requirements.txt`
+4. Get `iverilog`: `sudo apt-get update && sudo apt-get install iverilog`
+5. (Optional) Get `gtkwave` to see the signals as waveforms: `sudo apt-get install gtkwave`
 
 ## How to run
 
@@ -18,13 +21,19 @@ make -B
 
 To run gatelevel simulation, first harden your project and copy `../runs/wokwi/results/final/verilog/gl/{your_module_name}.v` to `gate_level_netlist.v`.
 
+This is in github actions. Go to the `Summary` page of github actions (top left, where you see the chip usage). Scroll down to the bottom until you see the `Artifacts` section and download `tt_submission`.
+The `{your_module_name}.v` will be in there.
+
 Then run:
 
 ```sh
-make -B GATES=yes
+export PDK_ROOT=./pdk  # Depending on where you execute this next line, it may be advised to use an absolute path. Replace '.' with this current directory
+volare enable --pdk "sky130" "cd1748bb197f9b7af62a54507de6624e30363943"
+GATES=yes make -B
 ```
 
 ## How to view the VCD file
+A `.vcd` file will be generated after you run a test.
 
 Using GTKWave
 ```sh
--- test/test_gates.py
+++ test/test_gates.py
@@ -26,6 +26,10 @@ async def test_project(dut):
     dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
+
+    # Power the device
+    dut.user_project.VPWR.value = 1
+    dut.user_project.VGND.value = 0
     await ClockCycles(dut.clk, 10)
 
     dut.rst_n.value = 1
@@ -49,7 +53,6 @@ async def test_project(dut):
 
     await RisingEdge(dut.clk)
     await ReadWrite()
-    await Timer(PERIOD/2)
 
     dut._log.info(f"io in: {dut.uio_in.value.binstr}")
 
@@ -63,48 +66,55 @@ async def test_project(dut):
 
     # Load operand A
     for i in range(4):
+        temp = struct.pack("<f", a)[i]
+        dut.ui_in.value = temp  # Put in little endian to input byte 0 first
+        # For some reason reading after awawiting ReadWrite() no longer works in the integration test,
+        # and we have to wait an extra period, even though the waveform is exactly the same
+        await RisingEdge(dut.clk)
+        await ReadWrite()
+
         assert dut.uio_out.value.binstr[0:4] == BinaryValue(i + 1, n_bits=4, bigEndian=False).binstr, f"Incorrect state when loading operand A byte {i}, uio_out: {dut.uio_out.value.binstr}"
         assert dut.uio_out.value.binstr[4] == '0', f"Done signal != 0, uio_out: {dut.uio_out.value.binstr}"
         dut._log.info(f"state: {dut.uio_out.value.binstr[0:4]}")
-        temp = struct.pack("<f", a)[i]
-        dut.ui_in.value = temp  # Put in little endian to input byte 0 first
         dut._log.info(f"Input: {BinaryValue(temp, n_bits=8, bigEndian=False)}")
-        await RisingEdge(dut.clk)
-        await ReadWrite()
         # dut._log.info(f"operand A: {dut.user_project.u_alu.operand_a.value.binstr}")
 
     # Load operand B
     for i in range(4):
+        temp = struct.pack("<f", b)[i]
+        dut.ui_in.value = temp  # Put in little endian to input byte 0 first
+        await RisingEdge(dut.clk)
+        await ReadWrite()
+
         assert dut.uio_out.value.binstr[0:4] == BinaryValue(i + 5, n_bits=4, bigEndian=False).binstr, f"Incorrect state when loading operand B byte {i}, uio_out: {dut.uio_out.value.binstr}"
         assert dut.uio_out.value.binstr[4] == '0', f"Done signal != 0, uio_out: {dut.uio_out.value.binstr}"
         dut._log.info(f"state: {dut.uio_out.value.binstr[0:4]}")
-        temp = struct.pack("<f", b)[i]
-        dut.ui_in.value = temp  # Put in little endian to input byte 0 first
         dut._log.info(f"Input: {BinaryValue(temp, n_bits=8, bigEndian=False)}")
-        await RisingEdge(dut.clk)
-        await ReadWrite()
         # dut._log.info(f"operand B: {dut.user_project.u_alu.operand_b.value.binstr}")
 
     # Wait for execute
-    assert dut.uio_out.value.binstr[0:4] == BinaryValue(9, n_bits=4, bigEndian=False).binstr, f"State != EXECUTE, uio_out: {dut.uio_out.value.binstr}"
-    assert dut.uio_out.value.binstr[4] == '0', f"Done signal != 0, uio_out: {dut.uio_out.value.binstr}"
-    dut._log.info(f"state: {dut.uio_out.value.binstr[0:4]}")
     # dut._log.info(f"result: {dut.user_project.u_alu.addsub_result.value.binstr}")
     await RisingEdge(dut.clk)
     await ReadWrite()
+    assert dut.uio_out.value.binstr[0:4] == BinaryValue(9, n_bits=4, bigEndian=False).binstr, f"State != EXECUTE, uio_out: {dut.uio_out.value.binstr}"
+    assert dut.uio_out.value.binstr[4] == '0', f"Done signal != 0, uio_out: {dut.uio_out.value.binstr}"
+    dut._log.info(f"state: {dut.uio_out.value.binstr[0:4]}")
 
     # Read outputs
     result = bytearray([0, 0, 0, 0])
     for i in range(4):
+        await RisingEdge(dut.clk)
+        await ReadWrite()
+
         result[i] = dut.uo_out.value.integer
         dut._log.info(f"byte {i}: {dut.uo_out.value.integer}, {dut.uo_out.value.binstr}")
         assert dut.uio_out.value.binstr[0:4] == BinaryValue(i + 10, n_bits=4, bigEndian=False).binstr, f"Incorrect state when reading result byte {i}, uio_out: {dut.uio_out.value.binstr}"
         assert dut.uio_out.value.binstr[4] == '1', f"Done signal != 1, uio_out: {dut.uio_out.value.binstr}"
         dut._log.info(f"state: {dut.uio_out.value.binstr[0:4]}")
-        await RisingEdge(dut.clk)
-        await ReadWrite()
 
     # Make sure returned to IDLE state
+    await RisingEdge(dut.clk)
+    await ReadWrite()
     assert dut.uio_out.value.binstr[0:4] == "0000", f"State != IDLE, uio_out: {dut.uio_out.value.binstr}"
     assert dut.uio_out.value.binstr[4] == '0', f"Done signal != 0, uio_out: {dut.uio_out.value.binstr}"
     dut._log.info(f"state: {dut.uio_out.value.binstr[0:4]}")
